932f9a7323531cbfb86837ca59657ee3dbe4f1ed
Vijay-dev01 <vijayajay3535@gmail.com>
2024-12-08 18:13:20 +0530

    error handled.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -16,14 +16,14 @@ const Home: React.FC = () => {
   return (
     <div className='sliding-img-container'>
       <div className='slider-body'>
-      <SimpleImageSlider
-        width={1000}
-        height={500}
-        images={images}
-        showBullets={false}
-        showNavs={true}
-      />
-      <p style={{color:"white"}}>hiusha oihhvakjsd hjvhakjdfv hjvhcjkfdv hjvkjcajdsv dkvbjadvda bjbvjbkjsbvbadvbbdavjbkb jbjkjsvj jbjvkbadbv jbkjbvsjbdf kjbjbvsjka</p>
+        <SimpleImageSlider
+          width={1000}
+          height={500}
+          images={images}
+          showBullets={false}
+          showNavs={true}
+        />
+        <p style={{ color: "white" }}>hiusha oihhvakjsd hjvhakjdfv hjvhcjkfdv hjvkjcajdsv dkvbjadvda bjbvjbkjsbvbadvbbdavjbkb jbjkjsvj jbjvkbadbv jbkjbvsjbdf kjbjbvsjka</p>
       </div>
     </div>
   )
