f4d352340bc80c0c15b80040b11424ed81592c70
Vijay-dev01 <vijayajay3535@gmail.com>
2024-12-17 16:11:42 +0530

    get payment order and get single order api.

--- server/routes/Order.js
+++ server/routes/Order.js
@@ -39,4 +39,61 @@ orderRouter.post(
   })
 );
 
+//order payment
+orderRouter.put(
+  "/:id/payment",
+  protect,
+  AsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        updated_time: req.body.updated_time,
+        email_address: req.body.email_address,
+      };
+      const updateOrder = await order.save();
+      res.status(200).json(updateOrder);
+    } else {
+      res.status(400);
+      throw new Error("Order Not Found!");
+    }
+  })
+);
+
+//Get All Orders
+orderRouter.get(
+  "/",
+  protect,
+  AsyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id }).sort({ _id: -1 });
+    if (orders) {
+      res.status(200).json(orders);
+    } else {
+      res.status(404);
+      throw new Error("Order Not Found!");
+    }
+  })
+);
+
+//Get Single Order
+orderRouter.get(
+  "/:id",
+  protect,
+  AsyncHandler(async (req, res) => {
+    const orders = await Order.findById(req.params.id).populate(
+      "user",
+      "email"
+    );
+    if (orders) {
+      res.status(200).json(orders);
+    } else {
+      res.status(404);
+      throw new Error("Order Not Found!");
+    }
+  })
+);
+
 module.exports = orderRouter;
